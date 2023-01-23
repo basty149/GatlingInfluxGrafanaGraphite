d1938de962bfff46aafe3e4aa107afa98b0c3db1
basty149 <basty149@gmail.com>
2023-01-23 14:55:31 +0100

    Use local GRAFANA PLUGINS for proxy issue

--- grafana/entrypoint.sh
+++ grafana/entrypoint.sh
@@ -8,7 +8,17 @@ post() {
         "$url$2" 2> /dev/null
 }
 
+if [ -z "${GF_INSTALL_PLUGINS}" ] 
+then
+  unzip -q /app/plugins/grafana-clock-panel-2.1.2.any.zip -d /var/lib/grafana/plugins/grafana-clock-panel
+  unzip -q /app/plugins/grafana-worldmap-panel-0.3.3.zip -d /var/lib/grafana/plugins/grafana-worldmap-panel
+  unzip -q /app/plugins/yesoreyeram-boomtable-panel-1.4.1.zip -d /var/lib/grafana/plugins/yesoreyeram-boomtable-panel
+  unzip -q /app/plugins/grafana-piechart-panel-1.6.4.any.zip -d /var/lib/grafana/plugins/grafana-piechart-panel
+fi
+
 if [ ! -f "/var/lib/grafana/.init" ]; then
+
+
     exec /run.sh $@ &
 
     until curl -s "$url/api/datasources" 2> /dev/null; do
